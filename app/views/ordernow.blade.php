672aebf66f6a5d701c793241accb2970dc387fcc
Marc Joseph Delim <Marc Joseph Delim>
2016-02-23 11:18:37 +0800

    final customer module as of now

--- app/views/ordernow.blade.php
+++ app/views/ordernow.blade.php
@@ -2,37 +2,6 @@
 
 @section('content')
 
-<<<<<<< HEAD
-	<div class="section white">
-		<div align = "center">
-			<div style= "height:30px;"></div>
-			<h4>Order Form</h4>
-		</div>
-
-		<div class = "container">
-			<div class = "container">
-				<div class = "card-panel">
-					<div class = "card-content">
-						<div class = "col s12">
-							<div class = "row">
-								<div class = "col s12">
-									<div class = "col s8">
-										<img style = "height:250px; width:200px; border: 1px dashed teal" src = "#!"/>
-									</div>
-									<div class = "col s4">
-
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-	</div>
-
-=======
 	<div class="section" style="background-image:url(img/background.jpg);">
 		<div align="center">
 			<h3 style="margin-top:30px">Order Form</h3>
@@ -73,9 +42,6 @@
 				</div>
 	    	</div>
 	    </div>
->>>>>>> 3863420b93843347ccf19ea11ddcad96c05a56de
-
-
 
     @stop
 
