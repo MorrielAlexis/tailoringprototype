75e6cc77938ecf00e62db1d3883eb518b4f5fc54
Marc Joseph Delim <Marc Joseph Delim>
2016-03-04 10:40:47 +0800

    update order tracking

--- app/views/orderTracking.blade.php
+++ app/views/orderTracking.blade.php
@@ -56,7 +56,7 @@
 							<span id="percent">60%</span>
 							<div id="bar"></div>
 						</div>
-					  	<label><center><font size ="+2">60% of the JOB is done</font></center></label>
+					  	<label><center><font size ="+2">30 of 50 of garments is done</font></center></label>
 					</div>
 				</div>
 			</div>
